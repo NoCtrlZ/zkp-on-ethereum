60099a188a12ba1b034774b86301c4536eef51f1
NoCtrlZ <phantomofrotten@gmail.com>
2020-11-02 18:02:41 +0900

    success compile

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Verifier = artifacts.require("Verifier");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Verifier);
 };
